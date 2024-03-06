24f0316aef267ef276fc9ec36e9eaaa25bf02e34
woldy <king@woldy.net>
2024-03-06 12:58:53 +0800

    暂存

--- finhack/server/default/default_server.py
+++ finhack/server/default/default_server.py
@@ -9,9 +9,20 @@ import runtime.global_var as global_var
 from finhack.library.mydb import mydb
 from flask import Flask, send_from_directory
 
-
-
-class DefaultTrader:
+class DefaultServer:
     def run(self):
-        
- 
+        app = Flask(__name__)
+
+        root_directory = REPORTS_DIR
+
+        @app.route('/<path:path>')
+        def static_proxy(path):
+            # send_static_file 会猜测正确的 MIME 类型
+            return send_from_directory(root_directory, path)
+
+        @app.route('/')
+        def redirect_to_index():
+            return send_from_directory(root_directory, 'index.html')
+
+        # 不再需要检查 __name__ == '__main__'，因为这个方法将被直接调用
+        app.run(debug=True)
